16600a695c28aff83739c2241fc47c3b2eb60c0f
Prashant Chaudhary <101.prashant@gmail.com>
2019-07-17 14:55:23 +0530

    setup routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {SurveyComponent} from './modules/app/components/survey/survey.component';
+import {ResultComponent} from './modules/app/components/result/result.component';
+import {NotFoundComponent} from './modules/app/components/not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SurveyComponent,
+  },
+  {
+    path: 'result/:id',
+    component: ResultComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import {
   MatButtonModule, MatRadioModule,
 } from '@angular/material';
+import { SurveyComponent } from './app/components/survey/survey.component';
+import { ResultComponent } from './app/components/result/result.component';
+import { NotFoundComponent } from './app/components/not-found/not-found.component';
 
 const modules = [
   MatButtonModule,
@@ -11,6 +14,7 @@ const modules = [
 @NgModule({
   imports: modules,
   exports: modules,
+  declarations: [SurveyComponent, ResultComponent, NotFoundComponent],
 })
 export class MaterialModule {
 }
